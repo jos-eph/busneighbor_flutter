da2510ef86d21846d1052d2d33ab5eed0e6405e0
Joe Mirarchi <joe@dydx.org>
2025-02-20 12:12:11 -0500

    Package GTFS data as a map

--- lib/service/gtfs-service.dart
+++ lib/service/gtfs-service.dart
@@ -1,6 +1,7 @@
 import 'package:http/http.dart';
-import 'package:protobuf/protobuf.dart';
 import 'package:busneighbor_flutter/model/gtfs-realtime/gtfs-realtime.pb.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:busneighbor_flutter/model/gtfsLocations.dart';
 
 final Uri SEPTA_LOCATIONS_GTFS = Uri.parse(
     "https://www3.septa.org/gtfsrt/septa-pa-us/Vehicle/rtVehiclePosition.pb");
@@ -34,18 +35,35 @@ class GtfsService {
     return response.bodyBytes;
   }
 
-  Future<void> printLocationsTest() async {
+  Map<int, Map<String, LatLng>> createDirectionIdLevel() {
+    return Map<int, Map<String, LatLng>>();
+  }
+
+  Map<String, LatLng> createVehicleLocationLevel() {
+    return Map<String, LatLng>();
+  }
+
+  Future<GtfsLocations> printLocationsTest() async {
     List<VehiclePosition> positions = await getPositions();
-    for (VehiclePosition position in positions) {
-      double lat = position.position.latitude;
-      double lng = position.position.longitude;
-      int direction = position.trip.directionId;
-      String route = position.trip.routeId;
-      String vehicleId = position.vehicle.id;
+    Map<String, VehiclePosition> vehicleIdToSource = Map();
+    Map<String, Map<int, Map<String, LatLng>>> locationsMap = Map();
+
+    for (VehiclePosition vehiclePosition in positions) {
+      double lat = vehiclePosition.position.latitude;
+      double lng = vehiclePosition.position.longitude;
+      int direction = vehiclePosition.trip.directionId;
+      String route = vehiclePosition.trip.routeId;
+      String vehicleId = vehiclePosition.vehicle.id;
+
+      vehicleIdToSource[vehicleId] = vehiclePosition; // set source
+
+      locationsMap.putIfAbsent(route, createDirectionIdLevel).putIfAbsent(
+          direction, createVehicleLocationLevel)[vehicleId] = LatLng(lat, lng);
 
       print("$lat\t$lng\t$direction\t$route\t$vehicleId");
     }
+    print("$locationsMap");
+
+    return GtfsLocations(locationsMap, vehicleIdToSource);
   }
-
-
 }
