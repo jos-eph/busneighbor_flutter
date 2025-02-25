19997fc91f7c19b5c88c60337146795594d9ee49
Joe Mirarchi <joe@dydx.org>
2025-02-25 09:33:34 -0500

    Stub location service

---
+++ lib/service/user-location-service.dart
@@ -0,0 +1,50 @@
+import 'package:geolocator/geolocator.dart';
+import 'package:latlong2/latlong.dart';
+
+class UserLocationService {
+  static LatLng CITY_HALL = LatLng(39.9528, -75.1635);
+
+  static Future<bool> locationServicesEnabled() async {
+    return await Geolocator.isLocationServiceEnabled();
+  }
+
+  // location services are automatically requested to be on when we request permission
+
+  static Future<bool> ensureLocationPermission() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.deniedForever) {
+      return false;
+    }
+
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied ||
+          permission == LocationPermission.deniedForever) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  static Future<Position> _determinePosition() async {
+    if (!await ensureLocationPermission()) {
+      return Future.error('Location permissions are denied');
+    }
+
+    // If permissions are granted, return the current location
+    return await Geolocator.getCurrentPosition();
+  }
+
+  static Future<LatLng> provideLocation() async {
+    Position position;
+
+    try {
+      position = await _determinePosition();
+    } catch (e) {
+      return CITY_HALL;
+    }
+
+    return LatLng(position.latitude, position.longitude);
+  }
+}
